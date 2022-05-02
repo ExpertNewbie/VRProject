2ddaeb050da54aad99c7e0378cd70cb098641e5a
CND <cndtjsqkr@gmail.com>
2022-05-02 22:39:44 +0900

    Create .gitattributes

---
+++ MyPortfolioVR/Assets/Scripts/RubberDuck/InvisibleController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c1902e862dc56d890f09bab17c11d61b505705b9fa6f0467467f4573cdfc33c6
+size 1895
---
+++ MyPortfolioVR/Assets/Scripts/TowerMoveController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:804a7802fec343b3ea5a42596f502cb13cafdc563168259f5218c26cebe46fc4
+size 1195
